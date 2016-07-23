ff45f8776b9aead7927404e0f536507beb9b9ec2
lishengzxc <eric@lishengcn.cn>
2016-07-23 20:17:47 +0800

    168

---
+++ 168.ExcelSheetColumnTitle.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+    var s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var result = '';
+
+    var num = '';
+    var eachNum = '';
+
+    while  (n > 0) {
+      num = n % 26;
+      if (num === 0) num = 26;
+      eachNum = s[num - 1];
+      result = eachNum + result;
+      n = ~~((n - 1) / 26);
+    }
+
+    return result;
+};
