702ea182a9c2ce24bc43e5e6b352f68ba715be22
lishengzxc <eric@lishengcn.cn>
2016-08-21 16:31:11 +0800

    242

---
+++ 242.ValidAnagram.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length !== t.length)
+    return false;
+
+  var a = s.split('').sort()
+    , b = t.split('').sort();
+
+  return a.toString() === b.toString();
+};
